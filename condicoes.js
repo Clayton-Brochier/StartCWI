b8bcbab310af4671407a44bd72308fef01eaa9bf
Clayton Brochier <claytonbrochier@hotmail.com>
2023-12-07 06:10:41 -0300

    Aula de condicionais acrescentado

---
+++ condicoes.js
@@ -0,0 +1,17 @@
+//condição simples
+var vel = 60.5;
+console.log(`A velocidade do seu veículo é de ${vel}km/h`);
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de volcidade da rodovia. Multado!`);
+} 
+    console.log('Continue com segurança!');
+
+
+//Condição composta
+var país = "EUA";
+console.log(`Vivendo em ${país}`);
+if (país == "Brasil") {
+    console.log("Você é brasileiro");
+} else {
+    console.log("Você é estrangeiro");
+}
